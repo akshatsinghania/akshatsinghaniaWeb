3e80cc450562c46c2f6f202aef0d9f6ffc153ceb
Akshat <singhaniaakshat1@gmail.com>
2022-12-25 02:46:10 +0530

    Added project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Components/Background";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 class="glitch">
+        <span>Hey I'm Akshat</span>
+        Hey I'm Akshat
+        <span>Hey I'm Akshat</span>
+      </h1>
+      <p>
+        This site is a work in progress and will be updated with my portfolio ,
+        blog and other stuff
+      </p>
+      <p href="mailto:singhaniaakshat1@gmail.com">singhaniaakshat1@gmail.com</p>
     </div>
   );
-}
+};
 
 export default App;
